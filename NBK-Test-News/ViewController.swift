00306770772a7be5b28b8abfca2b89fe6e751ee5
Bilal <bilalbakhrom@gmail.com>
2022-10-19 11:46:54 +0500

    Initial Commit

---
+++ NBK-Test-News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBK-Test-News
+//
+//  Created by Bilal Bakhrom on 19/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
