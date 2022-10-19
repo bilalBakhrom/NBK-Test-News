68ad15aa7bd9e567c82f83cb79e6764ed17a7566
Bilal <bilalbakhrom@gmail.com>
2022-10-19 12:32:20 +0500

    Add NewsSource codable model

---
+++ Packages/NewsNetwork/Sources/NewsNetwork/Data/Response/NewsSource.swift
@@ -0,0 +1,18 @@
+//
+//  NewsSource.swift
+//  
+//
+//  Created by Bilal Bakhrom on 19/10/2022.
+//
+
+import Foundation
+
+public struct NewsSource: Codable {
+    public var id: String
+    public var name: String
+    public var description: String
+    public var url: String
+    public var category: String
+    public var language: String
+    public var country: String
+}
